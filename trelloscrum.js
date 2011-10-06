ff0a74956a864b7627c56b5a9c9d1dd5aed74c39
paullofte <paul@lofte.net>
2011-10-06 14:49:06 -0700

    Added changing the list total based on the filter

--- trelloscrum.js
+++ trelloscrum.js
@@ -2,8 +2,10 @@
 //Adds Scrum to your Trello
 //Project by Jasper Kaizer <jasper@q42.nl> & Marcel Duin <marcel@q42.nl>
 function scoreCards(){
+    var filtered=$('.js-filter-cards').hasClass('is-on');
     $('div.list').each(function(){
         var list=$(this);
+        var total=0;
         list.find('.list-card').each(function(){
             var card = $(this);
             var point = 0;
@@ -15,30 +17,31 @@ function scoreCards(){
                     title.text(title.text().replace(/\(\d*\.?\d+\)\s?/,''));
                     found=true;
                 }
-                
-                // Look for unknown points cards through inclusion of (?) in title
-                if(!found){
+		// Unkown card support
+		if(!found){
                     point=title.text().replace(/^.*\((\?)\).*$/,'$1');
                     title.text(title.text().replace(/\((\?)\)\s?/,''));
 		    if('?'==point) found=true;
 		}
+
             });
-            // If a Point is found whether numeric or unkown add a badge
-            if(found)) {
+            if(found) {
                 card.find('.badges').each(function() {
                     var badge='<div class="badge"><span class="app-icon small-icon light point-icon badge-image">P</span><div class="badge-count point-count">'+point+'</div></div>';
                     $(this).append(badge);
                 });
             }
-        });
-    
-        var total=0;
-        list.find('.point-count').each(function(){
-            var point=Number($(this).text());
-            if(!isNaN(point)){
-                total += point;
-            }
-        });
+
+	    if(!filtered || (card.css('opacity')==1)){
+	        card.find('.point-count').each(function(){
+                    var point=Number($(this).text());
+                    if(!isNaN(point)){
+                        total += point;
+                    }
+                });
+	    }
+	});
+
 
         var _=list.find('.tot');
         if(!_[0])_=$('<span class="tot">').insertBefore(list.find('.list-header h2'));
