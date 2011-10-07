3b91b68f6072ae42f8968508ec77498e08575387
Nic Pottier <nicpottier@gmail.com>
2011-10-07 14:24:40 +0200

    add estimate picker to edit window
    Through some serious hackery, we figured out how to update the card title programmatically.  Now when you click the title on a card, a little menu will appear to let you pick the estimate for that card

--- trelloscrum.js
+++ trelloscrum.js
@@ -2,8 +2,38 @@
 //Adds Scrum to your Trello
 //Project by Jasper Kaizer <jasper@q42.nl> & Marcel Duin <marcel@q42.nl>
 
-function showPicker(badge){
+function replacePointValue(text, replacement){
+    var point = null;
 
+    if (replacement.length > 0){
+        replacement = "(" + replacement + ")";
+    }
+
+    // regular digits
+    match = text.match(/^.*\((\d*\.?\d+)\).*$/);
+    if (match != null){
+        point = Number(match[1]);
+        if (isNaN(point)){
+            point = null;
+        } else {
+            text = text.replace(/\((\d*\.?\d+)\)/, replacement);
+        }
+    }
+
+    // Unkown card support
+    if(point == null){
+        match = text.match(/^.*\((\?)\).*$/);
+        if (match != null){
+            point = '?';
+            text = text.replace(/\((\?)\)\s?/, replacement);
+        }
+    }
+
+    if (replacement.length > 0 && point == null){
+        text = text + " " + replacement;
+    }
+
+    return [text, point];
 }
 
 function scoreCards(){
@@ -13,24 +43,16 @@ function scoreCards(){
         var total=0;
         list.find('.list-card').each(function(){
             var card = $(this);
-            var point = 0;
-            var found =false;
+            var point = null;
+
             card.find('.list-card-title a').each(function(){
                 var title=$(this);
-                point=Number(title.text().replace(/^.*\((\d*\.?\d+)\).*$/,'$1'));
-                if(!isNaN(point)){
-                    title.text(title.text().replace(/\(\d*\.?\d+\)\s?/,''));
-                    found=true;
-                }
-		// Unkown card support
-		if(!found){
-                    point=title.text().replace(/^.*\((\?)\).*$/,'$1');
-                    title.text(title.text().replace(/\((\?)\)\s?/,''));
-		    if('?'==point) found=true;
-		}
-
+                var result = replacePointValue(title.text(), "");
+                title.text(result[0]);
+                point = result[1];
             });
-            if(found) {
+
+            if(point != null){
                 card.find('.badges').each(function() {
                     var badge='<div class="badge badge-points point-count" onclick="javascript:showPicker(this);">'+point+'</div>';
                     $(this).append(badge);
@@ -54,16 +76,42 @@ function scoreCards(){
     });
 }
 
+var _editorTitle = [];
+var _editControls = [];
+
+function updatePoint(){
+    var value = $(this).text();
+    var text = $(".card-detail-title .edit textarea").val();
+    $(".card-detail-title .edit textarea").remove();
+
+    // replace our new 
+    var result = replacePointValue(text, value);
+    text = result[0];
+
+    // total hackery to get Trello to acknowledge our new value
+    $(".card-detail-title .edit").prepend('<textarea type="text" class="field single-line" style="height: 42px; ">' + text + '</textarea>');
+
+    // then click our button so it all gets saved away
+    $(".card-detail-title .edit .js-save-edit").click();
+}
+
 $(function periodical(){
     scoreCards();
+
+    if ($(".card-detail-title .edit-controls").length > 0  && $(".card-detail-title .edit-controls .picker").length == 0){
+        var pickers = "";
+        pickers += '<span class="point-value">?</span> ';
+        pickers += '<span class="point-value">.5</span> ';
+        for (var i=1; i<=10; i++){
+            pickers += '<span class="point-value">' + i + '</span> ';
+        }
+        var picker = "<div class='picker'>" + pickers + "</div>";
+        $(".card-detail-title .edit-controls").append(picker);
+        $(".point-value").click(updatePoint);
+    }
+
     setTimeout(periodical,1000)
 })
 
 $(document).ready(function() {
-    var pickers = "";
-    for (var i=0; i<=5; i++){
-        pickers += '<span class="point-value">' + i + '</span> ';
-    }
-    var picker = "<div id='point-picker'>" + pickers + "</div>";
-    $("body").append(picker);
 });
