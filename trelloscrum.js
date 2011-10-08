f4711e3a0a549e53592b5644746a085e5f89f5aa
marcelduin <marcel@q42.nl>
2011-10-09 01:06:31 +0200

    js rewrite, now event-based

--- trelloscrum.js
+++ trelloscrum.js
@@ -1,55 +1,117 @@
-//TrelloScrum - https://github.com/marcelduin/TrelloScrum
-//Adds Scrum to your Trello
-//Project by Jasper Kaizer <jasper@q42.nl> & Marcel Duin <marcel@q42.nl>
-function scoreCards(){
-    var filtered=$('.js-filter-cards').hasClass('is-on');
-    $('div.list').each(function(){
-        var list=$(this);
-        var total=0;
-        list.find('.list-card').each(function(){
-            var card = $(this);
-            var point = 0;
-            var found =false;
-            card.find('.list-card-title a').each(function(){
-                var title=$(this);
-                point=Number(title.text().replace(/^.*\((\d*\.?\d+)\).*$/,'$1'));
-                if(!isNaN(point)){
-                    title.text(title.text().replace(/\(\d*\.?\d+\)\s?/,''));
-                    found=true;
-                }
-		// Unkown card support
-		if(!found){
-                    point=title.text().replace(/^.*\((\?)\).*$/,'$1');
-                    title.text(title.text().replace(/\((\?)\)\s?/,''));
-		    if('?'==point) found=true;
-		}
+/*
+** TrelloScrum v0.3 - https://github.com/Q42/TrelloScrum
+** Adds Scrum to your Trello
+**
+** Orig:
+** Jasper Kaizer <https://github.com/jkaizer>
+** Marcel Duin <https://github.com/marcelduin>
+**
+** Contribs:
+** Paul Lofte <https://github.com/paullofte>
+** Nic Pottier <https://github.com/nicpottier>
+**
+** Changelog
+**
+** v0.3
+** - Now event-driven, much faster response
+** - JS rewrite
+** - Small bugfixes
+**
+** v0.2
+** - Includes decimal story points
+** - Filtered cards aren't added to list totals
+** - Question mark accepted as story points
+** - Fancier storypoint display
+**
+** v0.1
+** - Initial release
+**
+**
+** Bugs:
+** - When deleting storypoints, it takes F5 to recalc
+**
+*/
 
-            });
-            if(found) {
-                card.find('.badges').each(function() {
-                    var badge='<div class="badge badge-points point-count">'+point+'</div>';
-                    $(this).append(badge);
-                });
-            }
+$(function(){
+	//watch filtering
+	$('.js-filter-cards').bind('DOMSubtreeModified',calcPoints);
 
-	    if(!filtered || (card.css('opacity')==1)){
-	        card.find('.point-count').each(function(){
-                    var point=Number($(this).text());
-                    if(!isNaN(point)){
-                        total += point;
-                    }
-                });
-	    }
-	});
+	//want: trello events
+	(function periodical(){
+		$('.list').each(list);
+		$('.list-card').each(listCard);
+		setTimeout(periodical,1000)
+	})();
+});
 
+//.list pseudo
+function list(e){
+	if(this.list)return;
+	this.list=true;
 
-        var _=list.find('.tot');
-        if(!_[0])_=$('<span class="tot">').insertBefore(list.find('.list-header h2'));
-        _.text(total>0?total:'')
-    });
+	var $list=$(this);
+	var $total=$('<span class="scrumTotal">')
+		.appendTo($list.find('.list-header h2'));
+
+	this.calc = function(){
+		var score=0;
+		$list.find('.list-card').each(function(){if(this.points)score+=this.points});
+		$total.text(score>0?score:'');
+	};
 }
 
-$(function periodical(){
-	scoreCards();
-	setTimeout(periodical,1000)
-})
+//.card-list pseudo
+function listCard(e){
+	if(this.scrumCard)return;
+	this.scrumCard=true;
+
+	var points=-1;
+	var that=this;
+	var $card=$(this);
+
+	if($card.hasClass('placeholder'))return;
+
+	$card.bind('DOMNodeInserted',function(e){
+		var nlist=$(that).closest('.list')[0];
+		if(e.target==that&&nlist){
+			getPoints();
+			printBadge();
+		}
+	});
+
+	var $badge=$('<span class="badge badge-points point-count">');
+
+	function printBadge(){
+		if($(that).parent()[0])$badge.insertBefore($(that).find('.badges').first())
+	};
+
+	function getPoints(){
+		var $title=$(that).find('.list-card-title a');
+
+		//accepts digits, decimals and question mark
+		var parsed=$title.text().replace(/^.*\((\?|\d*\.?\d+)\).*$/,'$1');
+
+		if((!isNaN(Number(parsed))&&parsed>=0)||parsed=='?'){
+			points=parsed;
+			$badge.text(points);
+			$title[0].otitle=$title.text();
+			$title.text($title.text().replace(/\((\?|\d*\.?\d+)\)\s?/,''));
+		}
+
+		calcPoints();
+	};
+
+	this.__defineGetter__('points',function(){
+		//don't add to total when filtered out
+		var filtered=$('.js-filter-cards').hasClass('is-on');
+		return (!filtered||$card.css('opacity')==1)&&points>=0?Number(points):0
+	});
+
+	getPoints();
+	printBadge();
+};
+
+//forcibly calculate list totals
+function calcPoints(){
+	$('.list').each(function(){if(this.calc)this.calc()})
+};
