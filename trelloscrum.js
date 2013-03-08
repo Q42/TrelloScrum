74f442e36425cfd34606383d3dae8ad624b758da
Carol <carol@carolglennon.com>
2013-03-08 12:00:57 -0500

    Revert "Version updated"
    This reverts commit 96dcf68c2744d5d40a201d7fb770d35d3bc335cb.

--- trelloscrum.js
+++ trelloscrum.js
@@ -13,15 +13,13 @@
 ** Morgan Craft <https://github.com/mgan59>
 ** Frank Geerlings <https://github.com/frankgeerlings>
 ** Cedric Gatay <https://github.com/CedricGatay>
-** Kit Glennon <https://github.com/kitglen
+**
 */
 
 //default story point picker sequence
 var _pointSeq = ['?', 0, .5, 1, 2, 3, 5, 8, 13, 20];
 //attributes representing points values for card
 var _pointsAttr = ['cpoints', 'points'];
-//the URL that gets prepended to any value in the Activity surrounded by * *
-var _OurURL = ['https://admin.wayfair.com/tracker/views/89.php?PrtID='];
 
 
 //internals
@@ -246,4 +244,76 @@ function showPointPicker() {
 	}))
 };
 
+
+//for export
+var $excel_btn,$excel_dl;
+window.URL = window.webkitURL || window.URL;
+window.BlobBuilder = window.BlobBuilder || window.WebKitBlobBuilder || window.MozBlobBuilder;
+
+function checkExport() {
+	if($('form').find('.js-export-excel').length) return;
+	var $js_btn = $('form').find('.js-export-json');
+	if($js_btn.length)
+		$excel_btn = $('<a>')
+			.attr({
+				style: 'margin: 0 4px 4px 0;',
+				class: 'button js-export-excel',
+				href: '#',
+				target: '_blank',
+				title: 'Open downloaded file with Excel'
+			})
+			.text('Excel')
+			.click(showExcelExport)
+			.insertAfter($js_btn);
+}
+
+function showExcelExport() {
+	$excel_btn.text('Generating...');
+
+	$.getJSON($('form').find('.js-export-json').attr('href'), function(data) {
+		var s = '<table id="export" border=1>';
+		s += '<tr><th>Points</th><th>Story</th><th>Description</th></tr>';
+		$.each(data['lists'], function(key, list) {
+			var list_id = list["id"];
+			s += '<tr><th colspan="3">' + list['name'] + '</th></tr>';
+
+			$.each(data["cards"], function(key, card) {
+				if (card["idList"] == list_id) {
+					var title = card["name"];
+					var parsed = title.match(reg);
+					var points = parsed?parsed[1]:'';
+					title = title.replace(reg,'');
+					s += '<tr><td>'+ points + '</td><td>' + title + '</td><td>' + card["desc"] + '</td></tr>';
+				}
+			});
+			s += '<tr><td colspan=3></td></tr>';
+		});
+		s += '</table>';
+
+		var bb = new BlobBuilder();
+		bb.append(s);
+		
+		var board_title_reg = /.*\/board\/(.*)\//;
+		var board_title_parsed = document.location.href.match(board_title_reg);
+		var board_title = board_title_parsed[1];
+
+		$excel_btn
+			.text('Excel')
+			.after(
+				$excel_dl=$('<a>')
+					.attr({
+						download: board_title + '.xls',
+						href: window.URL.createObjectURL(bb.getBlob('application/ms-excel'))
+					})
+			);
+
+		var evt = document.createEvent('MouseEvents');
+		evt.initMouseEvent('click', true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+		$excel_dl[0].dispatchEvent(evt);
+		$excel_dl.remove()
+
+	});
+
+	return false
+};
 
